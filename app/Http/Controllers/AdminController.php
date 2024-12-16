d621245e1976b33d711e1b924b3b46b162925d38
Muhammad Aka Sahadi <makasahadi14@gmail.com>
2024-12-17 06:15:06 +0700

    genosida

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -12,67 +12,58 @@ use Illuminate\Support\Facades\Hash;
 
 class AdminController extends Controller
 {
-    // Menampilkan form login
     public function showLoginForm()
     {
         return view('admin.login');
     }
 
-    // Proses login
+
     public function login(Request $request)
     {
-    // Validasi input username dan password
         $credentials = $request->validate([
             'username' => 'required',
             'password' => 'required',
         ]);
 
-        // Cari admin berdasarkan username
         $admin = \App\Models\Admin::where('username', $credentials['username'])->first();
 
-        // Cek apakah admin ditemukan dan password cocok
         if ($admin && Hash::check($credentials['password'], $admin->password)) {
-            // Simpan informasi admin ke sesi atau gunakan mekanisme login Laravel
-            // session(['admin_id' => $admin->admin_id]);
-            // Auth::login($admin);
             $request->session()->put('admin_logged_in', true);
             $request->session()->put('admin_id', $admin->admin_id);
-
-            // Redirect ke dashboard jika login berhasil
             return redirect()->route('admin.dashboard')->with('success', 'Login berhasil!');
         }
 
-            // Jika login gagal, beri pesan error
         return back()->with('error', 'Username atau Password salah');
     }
 
 
 
-    // Menampilkan halaman dashboard admin 
     public function dashboard()
     {
-        return view('admin.dashboard'); // pastikan Anda memiliki file view 'admin.dashboard'
+        return view('admin.dashboard'); 
     }
 
-    // Menampilkan halaman Manajemen Acara (ubah menjadi readAdminAcara)
+
     public function readAdminAcara()
     {
-        $acaras = Acara::all(); // Ambil semua data acara
+        $acaras = Acara::all();
         return view('admin.adminacara.readadminacara', compact('acaras'));
     }
 
+
     public function readAdminKoleksi()
     {
-        $koleksis = Koleksi::all(); // Ambil semua data koleksi 
-        return view('admin.adminkoleksi.readadminkoleksi', compact('koleksis')); // Kirim ke view
+        $koleksis = Koleksi::all(); 
+        return view('admin.adminkoleksi.readadminkoleksi', compact('koleksis')); 
     }
     
     
     public function readAdminTiket()
     {
-        return view('admin.admintiket.readadmintiket'); // Halaman untuk melihat acara
+        return view('admin.admintiket.readadmintiket'); 
     }
 
+    
     public function readAdminDonasi()
     {
         $donations = Donation::all(); 
--- app/Http/Controllers/TiketController.php
+++ app/Http/Controllers/TiketController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class AdminKoleksiController extends Controller
+class TiketController extends Controller
 {
     //
 }
